e75be4a378cff03fc798e5b73bccacf6160f88a9
Lina Rudashevski <lina.rudashevski@blinkhealth.com>
2019-08-02 19:02:59 -0400

    ok

--- selfies/settings.py
+++ selfies/settings.py
@@ -100,11 +100,12 @@ WSGI_APPLICATION = "selfies.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
-# DATABASES = {
-#     'default': dj_database_url.config(default=DATABASE_URL)
-# }
-
+# DATABASE_URL = os.environ.get("DATABASE_URL")
+# if DATABASE_URL:
+#     DATABASES = {
+#         'default': dj_database_url.config(default=DATABASE_URL)
+#     }
+# else:
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
